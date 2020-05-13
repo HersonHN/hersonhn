7228db298f59a8e50e7ca28c647063502f6402e0
Herson Salinas <hersonhn@gmail.com>
2020-05-13 16:57:23 -0600

    Webpack configuration and theme selector

---
+++ source/assets/src/global/index.js
@@ -0,0 +1,4 @@
+
+import { ThemeSelector } from './theme-selector/theme-selector';
+
+let ts = new ThemeSelector();
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,36 @@
+const SRC = './source/assets/src';
+
 module.exports = {
   mode: 'production',
+
   entry: {
-    earthbound: './source/assets/src/earthbound/index.js'
+    earthbound: `${SRC}/earthbound/index.js`,
+    global: `${SRC}/global/index.js`
   },
+
   output: {
     filename: '[name].js',
     path: __dirname + '/source/assets/dist'
   },
+
   module: {
     rules: [
-      { test: /\.js$/, use: 'babel-loader' },
-      { test: /\.dat$/, use: 'binary-loader' }
-    ]
+      { test: /\.dat$/, use: 'binary-loader' },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] },
+        },
+      },
+    ],
+  },
+
+  watch: true,
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
   }
 }
+
